6945be073af043ac223cc995b5c081284b179eb1
Benni-Math <benediktjens.arnarsson@gmail.com>
2024-01-19 11:20:24 -0500

    Adding flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:nix-community/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, pyproject-nix, ... }@inputs:
+    let
+      # Read pyproject.toml
+      pyproject = pyproject-nix.lib.project.loadPyproject { projectRoot = ./.; };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        python = pkgs.python3;
+        requirements = pyproject.renderers.withPackages { inherit python; };
+        pythonEnv = python.withPackages requirements;
+
+        pythonDist = pyproject.renderers.buildPythonPackage { inherit python; };
+      in
+      {
+        packages.default = python.pkgs.buildPythonPackage (pythonDist // {
+          # TODO: Update this to the latest before building, see ./marimo/__init__.py
+          version = "0.1.78";
+        });
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pythonEnv
+            nodejs
+            corepack
+          ];
+        };
+
+      }
+    );
+}
---
+++ scripts/nix-update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Shell script for updating the nix packaging
+# Make sure to run a manual `git fetch upstream && git rebase upstream/main`
+
+# git fetch upstream
+# git checkout main
+# git rebase upstream/main
+
+# Also check versions before running this
+
+nix develop . --show-trace --command $SHELL
+make fe
+git add .
+nix flake check . --show-trace --all-systems
+
+echo "Finished updating marimo derivation."
+echo "Remember to commit and push to make available through flake github:Benni-Math/marimo."
+echo "Then, run nix flake lock --update-input marimo in your project to pull in changes."
