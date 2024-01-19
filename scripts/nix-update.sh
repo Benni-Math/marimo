e37436731b31234dae4a03e9a3b808f15c0df615
Benni-Math <benediktjens.arnarsson@gmail.com>
2024-01-19 11:26:48 -0500

    Adding additional instructions to nix-update.sh

--- scripts/nix-update.sh
+++ scripts/nix-update.sh
@@ -15,5 +15,5 @@ git add .
 nix flake check . --show-trace --all-systems
 
 echo "Finished updating marimo derivation."
-echo "Remember to commit and push to make available through flake github:Benni-Math/marimo."
+echo "Remember to commit and (force) push to make available through flake github:Benni-Math/marimo."
 echo "Then, run nix flake lock --update-input marimo in your project to pull in changes."
